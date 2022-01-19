5566f80e4c539cdc9bf9545056c38ce5a98d6ffb
AnDev58 <andev58@outlook.com>
2022-01-19 12:43:01 +0300

    Added React support

--- src/app.js
+++ src/app.js
@@ -6,19 +6,19 @@ import homeRouter from "./router/homeRouter.js"
 const app = express()
 
 app.engine("hbs", engine({
-    layoutsDir: __dirname+"/../resources/view/layouts",
+    layoutsDir: __dirname+"/../../resources/view/layouts",
     defaultLayout: "main",
     extname: "hbs"
 }))
 
 app.set("view engine", "hbs");
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/../resources/view")
-registerPartials(__dirname + "../resources/view/partials");
+app.set("views", __dirname + "/../../resources/view")
+registerPartials(__dirname + "../../resources/view/partials");
 
 app.use(urlencoded({ extended: false }));
 
-app.use("/static", express.static(__dirname+"/../resources/static"))
+app.use("/static", express.static(__dirname+"/../../resources/static"))
+app.use("/static/js", express.static(__dirname+"/js"))
 
 app.use("/", homeRouter)
 
