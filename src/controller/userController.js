8c52b0c3289b36c3c3513cb630c2ee63a8ba5d7d
MB-Proger <miginandrey@outlook.com>
2022-02-14 15:48:58 +0300

    Fixed CSS

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,7 @@
 export const singup = function(request, response) {
-    response.render("singup.hbs", {
+    response.render("form.hbs", {
         "other_styles": ["form"],
+        "jquery": true,
         "use_awesome": true,
         "other_scripts": ["form"]
     })
