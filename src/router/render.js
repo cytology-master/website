ad7bec72bd060de9d6dfa4015aa77b4354ada582
AnDev <andev@localhost.localdomain>
2022-02-20 21:32:56 +0300

    Save

--- src/router/render.js
+++ src/router/render.js
@@ -1,7 +1,17 @@
-import { readdirSync } from fs
+import { lstatSync, readdirSync } from "fs"
+import { basename, join } from "path"
 
-function createMap(path) {
+function getFullPath(name) {
+    return join(__dirname, "../../resources/map", prefix, name)
+}
+
+function createMap(path, prefix) {
     const map = readdirSync(path).map(function(name) {
-        
+        var el = {
+            name: basename(name),
+            path: getFullPath(name),
+            url: "/"+prefix+"/"+name,
+            isFile: lstatSync(getFullPath(name)).isFile()
+        }
     })
 }
