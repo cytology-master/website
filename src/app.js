8b98cf90ce25fee850a4b7ba7b8e8226df57a05b
MB-Proger <miginandrey@outlook.com>
2022-02-21 15:24:17 +0300

    Commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express, { urlencoded } from "express" //express
 import { engine } from "express-handlebars" //layout for handlebars
 import { registerPartials } from "hbs" //Exactly handlebars
+import { join } from "path" // for map
 // Router importing
-import homeRouter from "./router/homeRouter.js" 
 import userRouter from "./router/userRouter.js"
-// import dump from "./router/render.js"
+import homeRouter from "./router/homeRouter.js"
+
 const app = express() //Initialising Express app
 
 // Views
@@ -30,9 +31,8 @@ app.use("/lib", express.static(__dirname+"/../lib"))
 app.use("/lib/jquery", express.static(__dirname+"/../node_modules/jquery/dist"))
 
 // Routers
-app.use("/users", userRouter)
 app.use("/", homeRouter)
-
+app.use("/users", userRouter)
 // 404 (Not found) page
 app.use(function (req, res, next) {
     res.status(404).send("Not Found<br><a href=\"/\">Back to main page</a>");
